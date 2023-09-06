5f6075b00b50ab8e1c81ad8aaebfa1a9d40b2b4e
Anilkumar K <89409899+anilk4@users.noreply.github.com>
2023-09-07 01:07:10 +0530

    Time: 138 ms (43.50%) | Memory: 32.3 MB (14.97%) - LeetSync

---
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        st = set()
+        while headA != None:
+            st.add(headA)
+            headA = headA.next
+        while headB != None:
+            if headB in st:
+                return headB
+            headB = headB.next
+        return None
+#we need to compare the  address of every pointer not value in linked lists
+
+'''while headB != None:
+            temp = headA
+            while temp != None:
+                if temp == headB:
+                    return headB
+                temp = temp.next
+            headB = headB.next
+        return None'''
